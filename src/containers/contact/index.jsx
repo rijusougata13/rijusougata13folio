9f7611ab2f148bf2bd5489d4d76052c54ced18c8
rijusougata13 <rijusougata13@gmail.com>
2021-03-13 10:46:02 +0530

    contact me

--- src/containers/contact/index.jsx
+++ src/containers/contact/index.jsx
@@ -11,10 +11,10 @@ const contact=(props)=>{
             <Wrappers>
                 <ul>
                 <li onClick={event =>  window.location.href='https://github.com/rijusougata13'}> <FaGithub size='2rem'/></li>
-                <li onClick={event =>  window.location.href='https://www.facebook.com/sougatadas.das.946'}><FaFacebook size='2rem'/></li>
-                   <li onClick={event =>  window.location.href='https://www.instagram.com/sougata_013/'}><FaInstagram size='2rem'/></li>
-                  <li onClick={event =>  window.location.href='https://www.linkedin.com/in/sougata-das-0a477016a/'} ><FaLinkedin size='2rem'/></li>
-                <li onClick={event =>  window.location.href='https://twitter.com/sougata13072001'} ><FaTwitter size='2rem'/></li>
+                <li onClick={event =>  window.location.href='https://www.facebook.com/rijusougata13'}><FaFacebook size='2rem'/></li>
+                   <li onClick={event =>  window.location.href='https://www.instagram.com/rijusougata13/'}><FaInstagram size='2rem'/></li>
+                  <li onClick={event =>  window.location.href='https://www.linkedin.com/in/rijusougata13/'} ><FaLinkedin size='2rem'/></li>
+                <li onClick={event =>  window.location.href='https://twitter.com/rijusougata13'} ><FaTwitter size='2rem'/></li>
                        <li onClick={event =>  window.location.href='mailto:rijusougata13@gmail.com'} ><img src={Mail}></img></li>            
                 </ul>
             </Wrappers>
